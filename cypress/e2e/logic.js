5a44e92d8db49b76e1a9f46c74321f2fcd313cf5
valdiviagm <thischarmingquark@gmail.com>
2024-05-02 14:35:12 -0600

    Add firt spec succesfully passed

--- cypress/e2e/logic.js
+++ cypress/e2e/logic.js
@@ -1,162 +1,205 @@
-function script() {
-    
-    let debug = true
+let debug = true
 
-        var doc;
-        var iframe = document.querySelector('iframe.aut-iframe'); // Adjust the selector as needed
+function findFactorByExactMatch(map, input ) {
+       
+    for (const key in map) {
         
-        if (iframe && iframe.contentWindow && iframe.contentWindow.document) {
-            doc = iframe.contentWindow.document;
-        } else {
-            doc = document; // Fallback to the main document if iframe is not accessible
+        const variations = key.split("|");
+       
+        if (variations.some(variation => variation.replaceAll("?", "") === input)) {
+            return map[key];
         }
+    }
+    return null; 
+}
+
+function getDocument() {
+
+    var doc;
+    var iframe = document.querySelector('iframe.aut-iframe'); // Adjust for Cypress testing
+    
+    if (iframe && iframe.contentWindow && iframe.contentWindow.document) {
+        doc = iframe.contentWindow.document;
+    } else {
+        doc = document;
+    }
+
+    return doc
+
+}
+
+const unitFactorMap = {
+    "l|L|litros?|LITROS?|Litros?": 1,
+    "ml": 0.001,
+    "kg|kilos?" : 1,
+    "g": 0.001,
+    "mgs?": 0.000001,
+    "pza?s?|Piezas?|pastillas?|bolsas?|rollos?|hojas?":1
+};
+
+function parseQuantityAmountUnit(ItemDescriptionBeforeParsingData) {
         
-        function injectPricePerUnit( priceItem, pricePerUnit ) {
+        
+    const unitPattern = Object.keys(unitFactorMap).join('|');
+    const regexPattern = new RegExp(
+        `(\\d+ (?:\\.\\d* )?)\\s*(${unitPattern})`
+    );
+    const match = Array.from(ItemDescriptionBeforeParsingData.matchAll(
+        new RegExp(regexPattern, 'g')
+    ));
+
+    if(debug){ 
+        console.log(
+            'match: ',
+            regexPattern,
+            match,
+            match.length ); 
+    }
+
+    
+    let amount;
+    let quantity;
+    let unit;
+
+    switch (true) {
+
+        case match.length == 1:
+
             
-            let dataLabelAttribute = 'pricePerUnit';
-            let pricePerUnitDiv = priceItem.querySelector(`div[data-label="${dataLabelAttribute}"]`);
-          
-            if (pricePerUnitDiv) {
-              pricePerUnitDiv.textContent = `${pricePerUnit}`;
-            } else {
-              pricePerUnitDiv = doc.createElement('div');
-              pricePerUnitDiv.setAttribute('data-label', dataLabelAttribute);
-              pricePerUnitDiv.textContent = `${pricePerUnit}`;
-              priceItem.appendChild(pricePerUnitDiv);
-            };
-            
-        };
-        
-        let unitFactorMap = {
-                "l|L|litros?|LITROS?|Litros?": 1,
-                "ml": 0.001,
-                "kg|kilos?" : 1,
-                "g": 0.001,
-                "mgs?": 0.000001,
-                "pza?s?|Piezas?|pastillas?|bolsas?|rollos?|hojas?":1
-        };
-        
-        function parseQuantityAmountUnit(ItemDescriptionBeforeParsingData) {
-        
-        
-            const unitPattern = Object.keys(unitFactorMap).join('|');
-            const regexPattern = new RegExp(
-                `(\\d (?:\\.\\d )?)\\s*(${unitPattern})`
-            );
-            const match = [...ItemDescriptionBeforeParsingData.matchAll(
-                new RegExp(regexPattern, 'g')
-            )];
-        
-            if(debug){ 
-                console.group(
-                    regexPattern,
-                    match,
-                    match.length ); 
+            let [ , quantity, unit] = match[0];
+
+            amount = findFactorByExactMatch( unitFactorMap, unit )
+
+            if( quantity == undefined || unit == undefined ){
+                throw new Error(
+                    "On case '1' quantity or unit 'undefinied'"
+                );
             }
-        
             
-            let amount;
-            let quantity;
-            let unit;
-        
-        
             
-          switch (true) {
-        
-                
-                  
-            case match.length == 1:
-        
-                
-                let [ , quantity, unit] = match[0];
-        
-                amount = findFactorByExactMatch( unitFactorMap, unit )
-        
-                if( quantity == undefined || unit == undefined ){
-                    throw new Error(
-                        "On case '1' quantity or unit 'undefinied'"
-                    );
-                }
-                
-                  
-                quantity = parseFloat(quantity )
-        
-                return [quantity * amount , 1 , unit]   
-                break;
-        
-            case match.length == 2:
-        
-        
-                let [ , quantityA, unitA] = match[0];
-                let [, quantityB, unitB] = match[1];
-        
-                  
-                amount = findFactorByExactMatch( unitFactorMap, unitB )
-        
-                  
-                  
-                if( quantityA == undefined || unitA == undefined || quantityB == undefined || unitB == undefined ){
-                    throw new Error(
-                        "On case '2' quantities or units 'undefinied'"
-                    );
-                }  
-                
-                quantityA = parseFloat( quantityA )
-                quantityB = parseFloat( quantityB )
-        
-                if(debug){
-                   console.log(quantityA, unitA, quantityB, unitB, "amount: ", amount ) 
-                }
-                  
-                return [quantityB * amount, quantityA, unitB]
-                break;
+            quantity = parseFloat(quantity )
+
+            return [quantity * amount , 1 , unit]
+
+        case match.length == 2:
+
+
+            let [, quantityA, unitA] = match[0];
+            let [, quantityB, unitB] = match[1];
+
             
-            default:
-        
-                   throw new Error("Unsupported input");
-                  
-          } 
-        
-        }
-        
-        
-        function getPricePerUnit( priceContainer ) {
+            amount = findFactorByExactMatch( unitFactorMap, unitB )
+
+
+            if( quantityA == undefined || unitA == undefined || quantityB == undefined || unitB == undefined ){
+                throw new Error(
+                    "On case '2' quantities or units 'undefinied'"
+                );
+            }  
             
-        
-            let priceItemSelector = '[data-automation-id="product-price"]';
-            let amountItemSelector = '[data-automation-id="product-title"]';
-            let price = priceContainer.querySelector(priceItemSelector).innerText.match(/\d /)[0];
-            let ItemDescriptionBeforeParsingData = priceContainer.querySelector(amountItemSelector).innerText;
-        
-        
-            let [amount, quantity, unit] = parseQuantityAmountUnit(ItemDescriptionBeforeParsingData);
-        
-            let totalQuantity = amount * quantity;
-            let pricePerUnit = parseFloat( (price / totalQuantity).toFixed(2) ) ;
-        
+            quantityA = parseFloat( quantityA )
+            quantityB = parseFloat( quantityB )
+
             if(debug){
-                
-                console.groupEnd(
-                `Info: ${ItemDescriptionBeforeParsingData},
-            price: ${price}, amount: ${amount}, quantity: ${quantity}, unit: ${unit}, totalQuantity: ${totalQuantity}, pricePerUnit: ${pricePerUnit}`);
-            }    
+            console.log(quantityA, unitA, quantityB, unitB, "amount: ", amount ) 
+            }
             
-            return pricePerUnit;
-        };
-           
+            return [quantityB * amount, quantityA, unitB]
+        
+        default:
+
+            throw new Error("Unsupported input");
+            
+    } 
+
+}
+
+function getPricePerUnit( priceContainer ) {
+            
+    let priceItemSelector = '[data-automation-id="product-price"]';
+    let amountItemSelector = '[data-automation-id="product-title"]';
+    let price = priceContainer.querySelector(priceItemSelector).innerText.match(/\d+\.\d+/)[0];
+    let ItemDescriptionBeforeParsingData = priceContainer.querySelector(amountItemSelector).innerText;
+
+    let [amount, quantity, unit] = parseQuantityAmountUnit(ItemDescriptionBeforeParsingData);
+
+    let totalQuantity = amount * quantity;
+    let pricePerUnit = parseFloat( (price / totalQuantity).toFixed(2) ) ;
+
+    if(debug){
+        
+        console.log(
+        `Info: ${ItemDescriptionBeforeParsingData},
+    price: ${price}, amount: ${amount}, quantity: ${quantity}, unit: ${unit}, totalQuantity: ${totalQuantity}, pricePerUnit: ${pricePerUnit}`);
+    }    
+    
+    return pricePerUnit;
+};
+
+function changeBorderColor(
+    element,
+    color,
+    opacity = 0.75,
+    borderWidth = '4px',
+    borderStyle = 'solid'
+) {
+    
+  const colorMap = {
+    green: `rgba(0, 128, 0, ${opacity})`,
+    yellow: `rgba(255, 255, 0, ${opacity})`,
+    blue: `rgba(0, 0, 255, ${opacity})`,
+    red: `rgba(255, 0, 0, ${opacity})`,
+    // Add more colors here as needed
+  };
+
+  const borderColor = colorMap[color];
+
+  if (borderColor) {
+    if(element){  
+    element.style = `border: ${borderWidth} ${borderStyle} ${borderColor};`;
+    }
+  } else {
+    console.warn(
+        'Invalid color provided. Supported colors:',
+        Object.keys(colorMap).join(', ')
+    );
+  }
+}
+
+function injectPricePerUnit( priceItem, pricePerUnit ) {
+            
+    let dataLabelAttribute = 'pricePerUnit';
+    let pricePerUnitDiv = priceItem.querySelector(`div[data-label="${dataLabelAttribute}"]`);
+  
+    if (pricePerUnitDiv) {
+      pricePerUnitDiv.textContent = `${pricePerUnit}`;
+    } else {
+      pricePerUnitDiv = getDocument().createElement('div');
+      pricePerUnitDiv.setAttribute('data-label', dataLabelAttribute);
+      pricePerUnitDiv.textContent = `${pricePerUnit}`;
+      priceItem.appendChild(pricePerUnitDiv);
+    };
+    
+};
+
+
+
+
+function script() {
+               
         ( function (){
             
             let bestPricePerUnit = 1000;
             let worstPricePerUnit = 0;
             let bestProduct, worstProduct;
-            
-                
-            [...doc
-             .querySelectorAll('div[data-testid][class=""]')
-            ].forEach(priceContainer => {
+        
+            Array.from(
+                getDocument().querySelectorAll('div[data-testid][class=""]')
+            ).forEach(priceContainer => {
             
                 try {
-                
+               
+                    
                     let pricePerUnit = getPricePerUnit( priceContainer );
                     
                     if(pricePerUnit < bestPricePerUnit && pricePerUnit !== 0){
@@ -207,54 +250,6 @@ function script() {
           }
         }
         
-        function findFactorByExactMatch(map, input ) {
-        
-            if(debug){
-                console.log("map, input: ",map, input)
-            }
-            
-            for (const key in map) {
-                
-                const variations = key.split("|");
-                
-                if (variations.some(variation => variation.replaceAll("?", "") === input)) {
-                    return map[key];
-                }
-            }
-            return null; 
-        }
-        
-        
-        function changeBorderColor(
-            element,
-            color,
-            opacity = 0.75,
-            borderWidth = '4px',
-            borderStyle = 'solid'
-        ) {
-            
-          const colorMap = {
-            green: `rgba(0, 128, 0, ${opacity})`,
-            yellow: `rgba(255, 255, 0, ${opacity})`,
-            blue: `rgba(0, 0, 255, ${opacity})`,
-            red: `rgba(255, 0, 0, ${opacity})`,
-            // Add more colors here as needed
-          };
-        
-          const borderColor = colorMap[color];
-        
-          if (borderColor) {
-            if(element){  
-            element.style = `border: ${borderWidth} ${borderStyle} ${borderColor};`;
-            }
-          } else {
-            console.warn(
-                'Invalid color provided. Supported colors:',
-                Object.keys(colorMap).join(', ')
-            );
-          }
-        }
-
 }
 
 // To make this function available to other scripts or Cypress specs
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,56 +7,61 @@ describe('My Web Page Test', () => {
     });
     cy.visit('/milk.html');
 
-    cy.get('span[data-automation-id="product-title"]span[data-automation-id="product-title"]').eq(1)
-    .should('exist')  // Assert that the element exists
+    cy.get('span[data-automation-id="product-title"]span[data-automation-id="product-title"]')
+    .eq(1)
+    .should('exist')
     .then(($description) => {
       const description = $description.text();
-      cy.log('Product description: ', description); // Log the text of the price
-      expect(description).to.equal('Leche de vaca Granja 3 bolsas de 900 ml c/u'); // Assert the price format, assuming it starts with a dollar sign followed by digits
+      cy.log('Product description: ', description);
+      expect(description).to.equal('Leche de vaca Granja 3 bolsas de 900 ml c/u');
     });
 
-    // Read the external script file and inject it into the window
     cy.readFile('cypress/e2e/logic.js').then((scriptContent) => {
       cy.window().then((win) => {
-          // Evaluate the script in the context of the window
+          
           win.eval(scriptContent);
-          // Now you can call your function as it's part of the window object
+          
           win.script();
       });
   });
 
     cy.window().then((win) => {
     
-        // Access the document of the window
     const targetDocument = win.document;
 
-    // Check if the button already exists
     let app = targetDocument.querySelector('button#app');
     if (!app) {
-      // Create a new button element if not exists
+      
       app = targetDocument.createElement('button');
       app.id = 'app';
-      app.textContent = 'TEMP';
-      // Optionally make the button invisible and non-interactive
-      // app.style.opacity = '0';
-      // app.style.position = 'absolute';
+      app.textContent = '';
+      
+      app.style.opacity = '0';
+      app.style.position = 'absolute';
       // app.style.pointerEvents = 'none';
 
-      // Append the button to the DOM
       targetDocument.body.appendChild(app);
     }
 
-    // Define a function to be called on click
     function helloWorld() {
       console.log('Hello, World');
       console.log(document)
     }
 
-    // Attach the event directly
     app.onclick = helloWorld;
 
     });
 
     cy.get('button#app').click();
+
+    cy.get('[data-label="pricePerUnit"]')
+    .eq(1)
+    .should('exist')
+    .then(($pricePerUnitLabel) => {
+      const pricePerUnit = $pricePerUnitLabel.text();
+      cy.log('Price per unit: ', pricePerUnit);
+      expect(pricePerUnit).to.equal('15.93');
+    });
+
   });
 });
